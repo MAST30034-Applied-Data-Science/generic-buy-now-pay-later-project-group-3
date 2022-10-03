0432af5b7ebce781284226b50f52f789eeed6971
Prathyush <jay.prathyush3615@gmail.com>
2022-10-03 20:06:36 +1100

    added clean script

---
+++ scripts/ReadMe.md
@@ -0,0 +1,7 @@
+# Scripts
+
+The scripts are divided as seen in the folder.
+
+# `clean.py`
+
+The clean py script contains functions that are required to clean external and internal datasets
---
+++ scripts/clean.py
@@ -0,0 +1,26 @@
+import os
+import utils as u
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+class Clean():
+    """
+    Class dedicated for cleaning internal and external datasets
+    """
+
+    def __init__(self):
+        self.sp = (
+            SparkSession.builder.appName("Clean BNPL")
+            .config("spark.sql.session.timeZone", "+11")
+            .getOrCreate()
+        )
+
+    def __del__(self):
+        self.sp.stop
+        print("Clean Up Completed!")
+
+    def clean_tax_income(self):
+        # Update to include tax income data
+        tax_income = u.read_tables(self.sp, "tax_income", "c")
+        pass
