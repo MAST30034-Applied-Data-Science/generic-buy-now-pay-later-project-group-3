9807c776c4e705f78c1bc616086ac68a936e8e4c
Prathyush <jay.prathyush3615@gmail.com>
2022-10-03 20:18:37 +1100

    updated main with new layout

--- scripts/main.py
+++ scripts/main.py
@@ -1,38 +1,16 @@
-'''This is the main session to run all the other preprocess modules, as well as presenting a cmd interface for the 
-    client'''
-#steps:
-# 1, download the external dataset(if any) 
-# 2, outliers  
-# 3, feature engineering 
-# 4, join datasets as the final input to modeling stage
+# update requirements.txt
+import utils
 
-#Questions:
-#1, code for downloading packages in requirements.txt?
-#2, 
+from download import Download
+from clean import Clean
+from process import Process
 
-from symbol import continue_stmt
-import download as d
-import feature  as f
-import outlier as o
+def BNPL_ranker():
+    """
+    Main funtion that calls and runs all scripts. Please view terminal for output on progress.
+    """
+    # d = Download()
+    # c = Clean()
+    # p = Process()
+    pass
 
-
-print("start preparing raw datasets, type \"Y\" to continue.")
-continue_indicator = input()
-if continue_indicator == "Y":
-    print("hello")
-
-#download external dataset
-if d.download_external() != True:
-    raise ValueError("can't download external dataset")
-else:
-    print("all datasets ready")
-
-
-
-
-
-    
-
-
-
-    
