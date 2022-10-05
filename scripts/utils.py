a1abf01a56ee23ea6626c6cb74b7c9bb47cfe555
Loky <captainloky4@gmail.com>
2022-10-05 16:31:55 +1100

    delete unused functions

--- scripts/utils.py
+++ scripts/utils.py
@@ -100,14 +100,4 @@ def safety_check(parent_dir: str, dir_name = None):
 ##########################
 
 
-def createTaregetDir(curated_dir, relative_dir):
-    if  not os.path.exits(curated_dir + relative_dir):
-        os.makedirs(curated_dir + relative_dir)
-    return curated_dir + relative_dir
-
-def show_intermediate(spark,target_dir = None, target_file = None):
-    if target_dir == None and target_file == None:
-        raise ValueError("No intemediate results to show")
-    if target_dir != None:
-        print("show dir")
 
