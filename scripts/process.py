1699b1aaeef75e469b54c43d789bd49e7b1e52ff
Prathyush <jay.prathyush3615@gmail.com>
2022-10-17 00:46:13 +1100

    cleaned model and minor bugs

--- scripts/process.py
+++ scripts/process.py
@@ -73,7 +73,7 @@ class Process():
         Merge holiday data with transactions
         """
         dir = sys.argv[2] + '/'
-        holiday = u.read_tables(self.sp, dir + "holiday")
+        holiday = u.read_tables(self.sp, "holiday")
         return self.transactions.join(holiday, holiday.date == self.transactions.order_datetime, how="left").drop("date")
 
     def create_cust_growth_column(self, merchants, transactions):
@@ -137,7 +137,7 @@ class Process():
         """
         Function to use entropy of merchants based on postcode and monthly revenue
         """
-        entropy = self.sp.read.option("inferSchema", True).parquet("../data/tables/entropy")
+        entropy = u.read_tables(self.sp, "entropy")
         return self.merchants.join(entropy, on="merchant_abn")
 
     def merchant_lookup(self):
